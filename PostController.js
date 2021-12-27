8cdd4046d456e89c07774870a9e8b02014cd5c8c
SNIGERS <edcon@yandex.ru>
2021-12-27 19:17:14 +0300

    PostController finished

--- PostController.js
+++ PostController.js
@@ -35,14 +35,25 @@ class PostController {
   }
   async update(req, res) {
     try {
-
+      const post = req.body
+      if (!post._id) {
+        res.status(400).json({message: "ID не указан"})
+      }
+      const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true});
+      return res.json(updatedPost);
     } catch (e) {
       res.status(500).json(e);
     }
   }
   async delete(req, res) {
     try {
+      const {id} = req.params
 
+      if (!id) {
+        res.status(400).json({message: "ID не указан"})
+      }
+      const post = await Post.findByIdAndDelete(id);
+      return res.json(post);
     } catch (e) {
       res.status(500).json(e);
     }
